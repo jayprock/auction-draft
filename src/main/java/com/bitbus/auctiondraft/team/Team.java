432e32efdfe1fdc55b72dc993b31ec120bb60c88
JayProck <jprochnow1@gmail.com>
2019-08-25 15:14:18 -0400

    Add NFL teams

---
+++ src/main/java/com/bitbus/auctiondraft/team/Team.java
@@ -0,0 +1,26 @@
+package com.bitbus.auctiondraft.team;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Team {
+
+    @Id
+    @Column(name = "team_id")
+    private int id;
+
+    @Column(nullable = false, length = 50)
+    private String city;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 3)
+    private String abbreviation;
+
+}
