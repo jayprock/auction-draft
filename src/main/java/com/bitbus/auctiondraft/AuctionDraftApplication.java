8257a804154cfcac2daa91e13f3c4dcb6ca4320b
JayProck <jprochnow1@gmail.com>
2019-08-25 05:57:53 -0400

    Generate project - backend only

---
+++ src/main/java/com/bitbus/auctiondraft/AuctionDraftApplication.java
@@ -0,0 +1,13 @@
+package com.bitbus.auctiondraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionDraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionDraftApplication.class, args);
+	}
+
+}
